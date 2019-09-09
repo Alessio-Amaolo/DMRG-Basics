632190a1931c10a4f6de374d2f1634b9f35f9dff
Alessio <alessioamaolo@gmail.com>
2019-09-09 07:07:04 -0700

    Continued dmrg implementation. Indices mostly now take into account edge cases rather than 0 padding

--- dmrg.py
+++ dmrg.py
@@ -44,6 +44,7 @@ class MPS():
         Multiply s and v into the next tensor. Repeat to right normalize
         the entire MPS.
         '''
+        # TODO: Fix indices for the end
         for i in range(self.mps.shape[0]-1, 1, -1):
             right = self.mps[i] # Get tensor
             sh = right.shape
@@ -57,11 +58,13 @@ class MPS():
 
             self.mps[i-1] = np.einsum('uxl, Rx -> uRl', self.mps[i-1], v)
             self.mps[i-1] = np.einsum('uxl, Rx -> uRl', self.mps[i-1], s)
+        # TODO: Fix indices
 
     def leftNormalize(self, index):
         '''
         Left normalize the MPS starting at index and working to the right.
         '''
+        # TODO: Fix indices, currently can't left sweep!
         for i in range(index, self.mps.shape[0]-1):
             left = self.mps[i]
             sh = right.shape
@@ -91,6 +94,7 @@ class Solver():
         self.operator = mpo
         self.Rdict = {}
         self.Ldict = {}
+        self.storeShape = None
 
     def R_Builder(self):
         '''
@@ -100,48 +104,76 @@ class Solver():
         '''
         l = self.state.shape[0]
         last = self.state[l-1]
-        R = np.einsum('xrl,URxL -> URrlL', last, self.operator[l-1])
+        # Initially, last is indexed up,right,down,left (last only has ul)
+        # and operator is indexed up,right,down,left (no right at first)
+        R = np.einsum('xl,UxL -> UlL', last, self.operator[l-1])
         # last is now indexed "in reverse" d l r
-        # now R is indexed leftTop, rightTop, rightMiddle, rightBottom, leftBottom, leftMiddle
-        R = np.einsum('URrlL,xij -> ijRrlL', R, last)
+        R = np.einsum('xlL,xi -> lLi', R, last) #R = np.einsum('URrlL,xij -> ijRrlL', R, last)
+        # Now R is indexed bottomleft, middleleft, topleft = lLi
         self.Rdict[l-1] = R # might need to copy R, as it might put a pointer to R there
         for i in range(l-2, 0, -1):
             next = self.state[i]
-            temp = np.einsum('xrl,URxL -> URrlL', last, self.operator[i])
-            temp = np.einsum('URrlL,xij -> ijRrlL', temp, last)
-            R = np.einsum('xjRrlL,ixLlab -> ijRrab', R, temp)
+            temp = np.einsum('xrl,URxL -> URrlL', next, self.operator[i])
+            temp = np.einsum('URrlL,xij -> ijRrlL', temp, next)
+            # R = np.einsum('xjRrlL,ixLlab -> ijRrab', R, temp)
+            R = np.einsum('abc,icbalL -> lLi', R, temp)
             self.Rdict[i] = R
-
-    def L_Builder(self):
-        '''
-        Build all L for a single variational cycle.
-        '''
-        # TODO: Do this, same as R.
-        pass
 
     def findMatrix(self, index):
         '''
         Given an index, find the tensor to be used for the standard eigenvalue
-        problem.
+        problem. Return it as a matrix (only two legs)
         '''
         R = self.Rdict[index]
+        # R is indexed bottom, middle, top (=lLi)
+        if (index == 0):
+            matrix = np.einsum('lxi, UxD -> UilD', R, self.operator[index])
+            sh = matrix.shape
+            matrix = np.reshape(matrix, [sh[0]*sh[1], sh[2]*sh[3]])
+            return matrix
+
+        # L is indexed bottom, middle, top (=rRj)
         L = self.Ldict[index]
-        # TODO: Multiply them together with an extra state + O, return the tensor
-        pass
+        if (index == self.state.shape[0]-1):
+            matrix = np.einsum('rxj, UDx -> jUDr', L, self.operator[index])
+            sh = matrix.shape
+            matrix = np.reshape(matrix, [sh[0]*sh[1], sh[2]*sh[3]])
+            return matrix
+
+        matrix = np.einsum('lxi, UxDL -> UilDL', R, self.operator[index])
+        matrix = np.einsum('UilDx, rxj -> UilDrj', matrix, L)
+        matrix = np.einsum('UilDrj -> jUilDr', matrix)
+        sh = matrix.shape
+        matrix = np.reshape(matrix, [sh[0]*sh[1]*sh[2], sh[3]*sh[4]*sh[5]])
+        return matrix
 
     def standardEigen(self, tensor):
         '''
         Solve tensor1*tensor2 - \lambda * tensor2 = 0 for the smallest
         eigenvalue/eigentensor pair.
+
+        Actually, that is not necessary right now. For the moment, it just
+        takes a matrix and a vector and finds the smallest eigenvalue/eigenvector
+        pair.
         '''
-        # TODO: Do this.
-        pass
+        # TODO: Actually implement Davidson's Method.
+        e, v = np.linalg.eig(tensor)
+        m = np.amin(e)
+        v = v[np.where(e == m)]
+        return (m, v)
+
 
     def contract(self):
         '''
         Contract the entire network to find <\psi|O|\psi>
         '''
-        pass
+        R_Builder()
+        R = self.Rdict[0]
+        next = self.state[0]
+        temp = np.einsum('xr,URx -> URr', next, self.operator[0])
+        temp = np.einsum('xRr,xj -> jRr', temp, next)
+        R = np.einsum('jRr,jRr', R, temp)
+        return R
 
     def solve(self, num):
         '''
@@ -150,13 +182,25 @@ class Solver():
         '''
         for i in range(num):
             # Right sweep
+            # TODO: Implement left edge case for right sweep
+
             for j in range(0, self.state.shape[0]-1):
                 tensor = findMatrix(j)
-                self.state[j] = standardEigen(tensor)
+                eigen = standardEigen(tensor)
+                sh = self.state[j].shape
+                eigen[1] = np.reshape(eigen[1], [sh[0], sh[1], sh[2]])
+                self.state[j] =
+                # TODO: Implement this and add to Ldict
+
+            # TODO: Implement right edge case for right sweep
+
+            # TODO: Implement right edge case for left sweep
 
             # Left sweep
             for j in range(self.state.shape[0]-1, 0, -1):
                 tensor = findMatrix(j)
                 self.state[j] = standardEigen(tensor)
+
+            # TODO: Implement left edge case for left sweep
 
         return (self.state, contract())
